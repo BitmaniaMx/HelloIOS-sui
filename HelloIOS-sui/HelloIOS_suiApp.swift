6bc0b9dd7bc11b7ebcc372bdffd7652788223f16
rafaelgonzalez <rafael.glez@gmail.com>
2024-02-24 13:17:33 -0600

    Initial Commit

---
+++ HelloIOS-sui/HelloIOS_suiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloIOS_suiApp.swift
+//  HelloIOS-sui
+//
+//  Created by Rafael González on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloIOS_suiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
